c2194b937b6f40d518f4ab1f16427f05db2e0226
Sherzod <96307270+sherzod66@users.noreply.github.com>
2022-11-04 22:39:36 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -39,7 +39,7 @@ const moon = new THREE.Mesh(moonSphere, moonMaterial);
 moon.position.set(0, 20, 30)
 shapeEarth.add(moon)
 
-const ambientLight = new THREE.AmbientLight(0x111111);
+const ambientLight = new THREE.AmbientLight(0x333333);
 scene.add(ambientLight)
 
 const light = new THREE.DirectionalLight(0xffffff, 1)
